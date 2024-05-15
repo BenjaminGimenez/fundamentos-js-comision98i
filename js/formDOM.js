5a1eea276100ebe3c2fef0f137ce59c9460f7fee
Benjamin Gimenez <benjagimenez824@gmail.com>
2024-05-15 12:29:01 -0300

    ejercicio de DOM terminado en clases, usando eventos para manejar datos de un formulario

--- js/formDOM.js
+++ js/formDOM.js
@@ -11,20 +11,50 @@ const autor = {
   const contenedorTarjeta = document.querySelector('#contenedor_tarjeta');
   
   const crearTarjeta = () => {
-    const tarjeta = document.createElement('div');
-    tarjeta.className = 'card';
+    contenedorTarjeta.innerHTML = "";
+    const tarjeta = document.createElement("div");
+    tarjeta.className = "card";
   
-    let datosAutor = `<img src="${autor.imagen}" class="card-img-top" alt="${autor.nombre}">
-    <div class="card-body">
-      <h5 class="card-title">${autor.nombre}</h5>
-      <p class="card-text">${autor.descripcion}</p>
-      <a href="#" class="btn btn-primary">Ver más</a>
-    </div>`;
+    let datosAutor = /*html*/ `<div class="d-flex justify-content-center mt-2">
+    <img src=${autor.imagen} class="card-img-top w-50" alt=${autor.nombre}>
+    </div>
+      <div class="card-body">
+        <h5 class="card-title">${autor.nombre}</h5>
+        <p class="card-text">${autor.descripcion}</p>
+        <a href="#" class="btn btn-primary">Ver perfil</a>
+      </div>`;
   
     tarjeta.innerHTML = datosAutor;
   
-    contenedorTarjeta.appendChild(tarjeta);
-  }
+    contenedorTarjeta.append(tarjeta);
+  };
+  
+  //manejo de eventos
+  const modificarAutor = (event) => {
+    event.preventDefault();
+    console.log(event);
+    autor.nombre = document.querySelector("#nombre").value;
+    autor.descripcion = document.querySelector("#descripcion").value;
+  
+    crearTarjeta();
+    limpiarForm();
+  };
+  
+  const limpiarForm = () => {
+    document.querySelector("#formulario").reset();
+    document.querySelector("#nombre").focus();
+  };
   
   crearTarjeta();
+  
+  document
+    .querySelector("#formulario")
+    .addEventListener("submit", modificarAutor);
+  
+  
+  
+  
+  
+  
+  
   
